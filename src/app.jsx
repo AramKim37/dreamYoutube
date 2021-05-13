fa45ed7ddba8284df5bf84752e2c2b395fedf7a3
Aram Kim <nayulpapa0201@gmail.com>
2021-05-13 15:01:31 -0600

    display list

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
+import "./app.css";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAys6M6H4kaEky9wqjo-G6HvZ2YS-p2IyY",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
+}
+
+export default App;
